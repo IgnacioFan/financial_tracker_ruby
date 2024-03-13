cd446ae59748a5b2537ff54e888dbab1e5bdb158
Weilong <fan01856472@gmail.com>
2024-03-13 11:57:09 -0400

    add rspec

--- Gemfile
+++ Gemfile
@@ -51,5 +51,10 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+end
 
+group :test do
+  gem 'rspec', '~> 3.11'
+  gem 'rspec-rails', '~> 5.1'
+  gem 'factory_bot_rails', '~> 6.2'
 end
--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module FinancialTracker
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |gen|
+      gen.test_framework :rspec
+    end
   end
 end
