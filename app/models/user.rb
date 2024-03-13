e60763f9c4c56b8bd6003c999338f9d1d3d58f7f
Weilong <fan01856472@gmail.com>
2024-03-13 11:49:13 -0400

    test tailwind css ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  ROLE = {
+    admin: 1,
+    staff: 2,
+    client: 3
+  }
+
+  before_validation :set_client, :on => :create
+
+  validates_presence_of :name
+
+  private
+
+  def set_client
+    self.role = ROLE[:client]
+  end
 end
