ddc133057cac0ec4942040304be974110d8a5e53
Weilong <fan01856472@gmail.com>
2024-03-17 15:19:16 -0400

    remove view specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
+  let!(:user) { create(:user) }
+
+  before { sign_in(user) }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/dashboard/index"
+      get "/"
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
     end
   end
-
 end
