0d5930dfe4665a68aff475575f68659fac8ccd85
Weilong <fan01856472@gmail.com>
2024-03-22 17:16:48 -0400

    remove jbuilder code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,27 +20,19 @@ class ProductsController < ApplicationController
   def create
     @product = current_user.products.new(product_params) 
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to products_url, notice: "Product was successfully created." }
-        # format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      redirect_to products_url, notice: "Product was successfully created." 
+    else
+      render :new, status: :unprocessable_entity 
     end
   end
 
   # PATCH/PUT /products/1 or /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to products_url, notice: "Product was successfully updated." }
-        # format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update(product_params)
+      redirect_to products_url, notice: "Product was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -48,20 +40,17 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to products_url, notice: "Product was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :description, :price, :user_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :user_id)
+  end
 end
